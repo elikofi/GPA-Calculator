d0c78bc590188714b165c568c7646a9299ffce0e
elijah amoako <elijahamoako72@gmail.com>
2022-08-28 17:06:16 +0000

    first commit

---
+++ GPAcalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Welcome to my GPA Calculator App.");
+    Console.ResetColor();
+
+    Console.WriteLine("Choose an option:\n");
+    Console.WriteLine("\t1: Calculate GPA");
+    Console.WriteLine("\t2: Calculate CGPA");
+    string option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            Console.WriteLine();
+
+            break;
+        default:
+            break;
+    }
+}
+catch (Exception ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Enter a valid input.");
+}
+
