ad6a98eaf0d8cb8603ef6e7b168200ac765bb208
elijah amoako <elijahamoako72@gmail.com>
2022-08-28 17:29:04 +0000

    updated

--- GPAcalculator/Program.cs
+++ GPAcalculator/Program.cs
@@ -1,18 +1,33 @@
 ﻿try
 {
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Welcome to my GPA Calculator App.");
+    Console.WriteLine("Welcome to my GPA Calculator App.\n");
     Console.ResetColor();
 
-    Console.WriteLine("Choose an option:\n");
-    Console.WriteLine("\t1: Calculate GPA");
-    Console.WriteLine("\t2: Calculate CGPA");
+    Console.WriteLine("Choose an option:");
+    Console.WriteLine("\t\t1: Calculate GPA");
+    Console.WriteLine("\t\t2: Calculate CGPA");
     string option = Console.ReadLine();
 
+    int rounds = 0;
     switch (option)
     {
         case "1":
-            Console.WriteLine();
+            int numberOfCourses;
+            do
+            {
+                
+                Console.Write("How many courses did you offer?- ");
+                numberOfCourses = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter number of credit hour(s) for the course: ");
+                string creditHours = Console.ReadLine();
+
+                Console.Write("Enter your grade for this course: ");
+                string grade = Console.ReadLine();
+                rounds++;
+            } while (rounds < numberOfCourses);
+            
 
             break;
         default:
