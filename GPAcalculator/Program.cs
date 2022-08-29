2af3f03fff716219702e20e5e23edd94df9c3d3a
elijah amoako <elijahamoako72@gmail.com>
2022-08-29 06:47:32 +0000

    update

--- GPAcalculator/Program.cs
+++ GPAcalculator/Program.cs
@@ -1,4 +1,7 @@
-﻿try
+﻿int numberOfCourses, creditHours = 0;
+double fGPA, sGPA, tGPA, fthGPA = 0;
+
+try
 {
     Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("Welcome to my GPA Calculator App.\n");
@@ -9,17 +12,17 @@
     Console.WriteLine("\t\t2: Calculate CGPA");
     string option = Console.ReadLine();
 
-    int askAgain = 0;
+    
     switch (option)
     {
         case "1":
-            int numberOfCourses;
+            int askAgain = 0;
             Console.Write("How many courses did you offer?- ");
             numberOfCourses = Convert.ToInt32(Console.ReadLine());
             do
             {
                 Console.Write("Enter number of credit hour(s) for the course: ");
-                int creditHours = Convert.ToInt32(Console.ReadLine());
+                creditHours = Convert.ToInt32(Console.ReadLine());
 
                 Console.Write("Enter your grade for this course: ");
                 string grade = Console.ReadLine().ToUpper();
@@ -27,6 +30,22 @@
             } while (askAgain < numberOfCourses);
             
 
+            break;
+
+        case "2":
+            Console.Write("What was your GPA for first year: ");
+            fGPA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What was your GPA for second year: ");
+            sGPA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What was your GPA for third year: ");
+            tGPA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What was your GPA for fourth year: ");
+            fthGPA = Convert.ToDouble(Console.ReadLine());
+
+            double CGPA = ;
             break;
         default:
             break;
